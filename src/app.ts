2ce66d6c946c3b84d134aa873eea5aa1ed7c26e6
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-06-09 00:08:47 +0600

    interface of order and product declared

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express'
 import { ProductRoutes } from './app/modules/products/product.route';
 import { OrderRoutes } from './app/modules/orders/order.route';
+import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/products', ProductRoutes);
 app.use('/api/orders', OrderRoutes);
---
+++ src/app/modules/products/product.interface.ts
@@ -0,0 +1,17 @@
+type TVariant = {
+    type: string,
+    value: string
+}
+
+export type TProduct = {
+    name: string,
+    description: string,
+    price: number,
+    category: string,
+    tags: string[],
+    variants: TVariant[],
+    inventory: {
+        quantity: number,
+        inStock: boolean
+    }
+}
